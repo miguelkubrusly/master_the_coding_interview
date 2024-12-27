78b1df3223b82ba2423275ab7768ce80cb473fa8
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com.>
2024-12-26 22:27:30 -0300

    refactor(arr1): 💩 try binary search erroneously

--- arr1.js
+++ arr1.js
@@ -1,38 +1,59 @@
 "use strict";
-function solution(nums, target) {
-    if (nums.length < 2) {
-        return null;
-    }
-    // brute force solution
+function twoSum(nums, target) {
+    // BRUTE FORCE SOLUTION
+    // O(n**2) - O(1)
     // ------------------------ //
-    for (let p1 = 0; p1 < nums.length - 1; p1++) {
+    // for (let p1 = 0; p1 < nums.length-1; p1++) {
+    //   const numToFind = target - nums[p1]
+    //   for (let p2 = p1+1; p2 < nums.length; p2++) {
+    //     if (nums[p2] == numToFind){
+    //       return [p1,p2]
+    //     }
+    //   }
+    // }
+    // return null
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    // OPTIMAL SOLUTION
+    // O(n) - O(1)
+    // ------------------------ //
+    for (let p1 = 0; p1 < nums.length; p1++) {
         const numToFind = target - nums[p1];
-        for (let p2 = p1 + 1; p2 < nums.length; p2++) {
-            if (nums[p2] == numToFind) {
-                return [p1, p2];
+        const sortedNums = nums.slice(p1 + 1).sort((a, b) => a - b);
+        let start = 0, end = sortedNums.length - 1;
+        while (start < end) {
+            const middle = Math.floor(end - start / 2);
+            const middle_num = sortedNums[middle];
+            if (middle_num < numToFind) {
+                start = middle + 1;
+            }
+            else if (middle_num > numToFind) {
+                end = middle - 1;
+            }
+            else {
+                return [p1, nums.indexOf(middle_num)];
             }
         }
     }
     return null;
 }
 // tests
-const a = solution([1, 2, 4, 5, 6], 9);
-console.log('====================================');
-console.log('Should be [2,3]:');
+const a = twoSum([1, 2, 4, 5, 6], 9);
+console.log("====================================");
+console.log("Should be [2,3]:");
 console.log(a);
-const b = solution([1, 8, 23, 2, 45, 4, 11, 2], 11);
-console.log('====================================');
-console.log('Should be null:');
+const b = twoSum([1, 8, 23, 2, 45, 4, 11, 2], 11);
+console.log("====================================");
+console.log("Should be null:");
 console.log(b);
-const c = solution([], 0);
-console.log('====================================');
-console.log('Should be null:');
+const c = twoSum([], 0);
+console.log("====================================");
+console.log("Should be null:");
 console.log(c);
-const d = solution([2, 4, 6, 7], 9);
-console.log('====================================');
-console.log('Should be [0,3]:');
+const d = twoSum([2, 4, 6, 7], 9);
+console.log("====================================");
+console.log("Should be [0,3]:");
 console.log(d);
-const e = solution([100, 101, 102, 2], 102);
-console.log('====================================');
-console.log('Should be [0,3]:');
+const e = twoSum([100, 101, 102, 2], 102);
+console.log("====================================");
+console.log("Should be [0,3]:");
 console.log(e);
