a8d036776cef290d8176baaf16506a8d496fee59
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-12-30 15:40:19 -0300

    fix(arr3): ⚡️ improve and pass test on leetcode

--- arr3.ts
+++ arr3.ts
@@ -1,18 +1,21 @@
-function trap(heights: number[]) {
-  let totalWater = 0;
-  // brute force
-  for (let i = 1; i < heights.length - 1; i++) {
-    const h = heights[i];
-    let waterCol = 0;
-    const maxBefore = Math.max(...heights.slice(0, i));
-    const maxAfter = Math.max(...heights.slice(i + 1));
-    if (maxBefore > h && maxAfter > h) {
-      waterCol = Math.min(maxBefore, maxAfter) - h;
-      totalWater += waterCol;
+function trap(height: number[]) {
+  let res = 0;
+  let maxAfter = Math.max(...height);
+  let maxBefore = 0;
+
+  for (let i = 0; i < height.length - 1; i++) {
+    const h = height[i];
+    if (h === maxAfter) {
+      maxAfter = Math.max(...height.slice(i + 1));
     }
+    if (maxBefore > h && maxAfter > h) {
+      const waterCol = Math.min(maxBefore, maxAfter) - h;
+      res += waterCol;
+    }
+    maxBefore = Math.max(maxBefore, h);
   }
 
-  return totalWater;
+  return res;
 }
 
 console.log("Should be 8:");
@@ -33,7 +36,7 @@ console.log("=================");
 console.log("Should be 0:");
 console.log(trap([]));
 console.log("=================");
-console.log("Should be...");
+console.log("Should be... 129469!");
 console.log(
   trap([1213, 232, 54, 123, 832748, 222, 123114, 133, 4, 123, 1, 902])
 );
